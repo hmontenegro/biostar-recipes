d1a6ef55d057421f44aa4e4f6ca1f33a0b2c8c80
Istvan Albert <istvan.albert@gmail.com>
2018-01-23 13:55:12 -0500

    Update by ialbert on Tue Jan 23 13:55:12 EST 2018 from yolo.local

--- Makefile
+++ Makefile
@@ -1,3 +1,31 @@
+USER=www
+DATA_DIR=/export/sites/main_data/initial
+DATA_HOST=data.bioinformatics.recipes
+
+# The initial data for all recipes
+DATA_FILE=recipes-initial-data.tar.gz
+
+all:
+	@echo Valid commands: push, data_pull
+
+# The local directory
+export/local:
+	mkdir -p export/local
+
+# Make the required directories
+dir: export/local
+
 push:
 	git commit -am "Update by `whoami` on `date` from `hostname`"
 	git push
+
+mothur:
+	python manage.py project --json projects/metagenome/mothur-project.hjson --privacy public --jobs
+
+data_pull: dir
+	(cd export && rsync -avz ${USER}@${DATA_HOST}:${DATA_DIR}/${DATA_FILE} . )
+	(cd export && tar zxvf ${DATA_FILE})
+
+pack: dir
+	(cd export && tar czvf ${DATA_FILE} local/*)
+	(cd export && rsync -avz ${DATA_FILE} ${USER}@${DATA_HOST}:${DATA_DIR}/)
--- recipes/alignment/bwa.sh
+++ recipes/alignment/bwa.sh
@@ -1,68 +1,33 @@
 set -ueo pipefail
 
-# Get parameters.
-INPUT={{sequence.value}}
+# Obtain the parameters.
+INPUT={{reads.value}}
 GENOME={{genome.value}}
-URL={{runtime.job_url}}
 
-# Internal parameters.
+# Build the BWA index
+mkdir -p index
+INDEX=index/{{genome.uid}}
 
-# Bwa index.
-INDEX_DIR=$(dirname ${GENOME})/bwa
-mkdir -p ${INDEX_DIR}
-INDEX=${INDEX_DIR}/{{genome.uid}}
+# This directory will store the alignment.
+mkdir -p bam
+BAM=bam/alignment.bam
 
-# Fasta index.
-GENOME_FAIDX=${GENOME}.fai
-
-# Work directory.
-WORK={{runtime.work_dir}}/work
-mkdir -p ${WORK}
-
-# Main results.
-RESULT_VIEW={{runtime.work_dir}}/{{settings.index}}
-OUTPUT=${WORK}/aligned.bam
-
-# Build bwa index if not exist.
-
-if [ ! -f "$INDEX.bwt" ]; then
-echo "Building bwa index."
+# Build the BWA index.
+echo "Build the bwa index."
 bwa index -p ${INDEX} ${GENOME}
-fi
 
 # Align sequences
+echo  "Aligning reads to the genome."
+bwa mem -t 4 ${INDEX} ${INPUT}  | samtools view -b |samtools sort >${BAM}
+samtools index ${BAM}
 
-echo  "Mapping reads to the genome."
-bwa mem -t 4 ${INDEX} ${INPUT}  | samtools view -b |samtools sort >${OUTPUT}
-samtools index ${OUTPUT}
+echo "Compute alignment statistics."
+samtools flagstat ${BAM} > flagstat.txt
+samtools idxstats ${BAM} > idxstats.txt
 
-echo "Computing statistics."
-echo "--------------------"
-samtools flagstat ${OUTPUT}
-echo "--------------------"
+# Show the statistics on the output.
+echo "-------- Flag Stats -------------"
+cat flagstat.txt
 
-# Get samtools flagstats results.
-samtools flagstat ${OUTPUT} >${WORK}/alignment_stats.txt
-
-# Get number of reads mapped to each chromosome.
-samtools idxstats ${OUTPUT} >${WORK}/chrom_mapping.txt
-
-echo "Generating plots."
-
-# Plot results.
-python -m biostar.tools.align.plotter ${WORK}/alignment_stats.txt ${WORK}/chrom_mapping.txt >${RESULT_VIEW}
-
-# Build genome index if not exist.
-if [ ! -f "$GENOME_FAIDX" ]; then
-echo "Building genome index."
-samtools faidx ${GENOME}
-fi
-
-# Link genome to workdir.
-ln -s $GENOME $WORK
-ln -s $GENOME_FAIDX $WORK
-
-# Generate igv visualization.
-echo "Generating IGV visualization file."
-python -m biostar.tools.igv.visualization $URL $WORK >igv.xml
-
+echo "-------- Index Stats ------------"
+cat idxstats.txt | head -100
