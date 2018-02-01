4520502f1fef7115ce17647b11acb2a6a5b30e6d
aswathy <aswathyseb@gmail.com>
2018-02-01 09:59:44 -0500

    Update by asebastian on Thu Feb  1 09:59:44 EST 2018 from Aswathys-iMac.local

--- recipes/rnaseq/kallisto-PE.sh
+++ recipes/rnaseq/kallisto-PE.sh
@@ -7,6 +7,9 @@ TRANSCRIPTS={{transcripts.value}}
 # Directory with Kallisto results.
 mkdir -p results
 
+# Directory with intermediate files.
+mkdir -p tmp
+
 # Build the Kallisto index.
 INDEX_DIR={{runtime.local_root}}/temp
 mkdir -p ${INDEX_DIR}
@@ -22,20 +25,18 @@ else
 fi
 
 # Calculate abundances using kallisto.
-cat ${INPUT}| egrep "fastq|fq" | sort | parallel -N 2 -j 4 kallisto quant -o results/{1/.}.out  -i ${INDEX} {1} {2}
+cat ${INPUT}| egrep "fastq|fq" | sort | parallel -N 2  -j 4 kallisto quant -o results/{1/.}.out  -i ${INDEX} {1} {2}
 
 # Create a combined abundance table for all samples.
 
-# Get all counts with est_count column renamed with sample_name
-ls -d results/* | cut -f 2 -d "/" |parallel "sed -e 1s/est_counts/{.}/ results/{}/abundance.tsv | cut -f 4 >{}_counts.txt"
+# Directory name is the sample name.
+# Rename est_count column to sample name and extract it into a new file.
+ls -d results/* | cut -f 2 -d "/" | parallel "sed -e 1s/est_counts/{.}/ results/{}/abundance.tsv | cut -f 4 >tmp/{}_counts.txt"
 
-# Combine counts from all samples.
-paste *counts.txt >all.txt
+# Combine all counts into a single file.
+paste tmp/*counts.txt > tmp/all.txt
 
-# Add  transcripts ids to the counts.
-ls -d results/* | head -1 | parallel cat {}/abundance.tsv | cut -f 1 | paste - all.txt > raw_abundance.txt
-
-# Remove intermediate files.
-rm -f all.txt *counts.txt
+# Add  transcript ids to the counts.
+ls -d results/* | head -1 | parallel cat {}/abundance.tsv | cut -f 1 | paste - tmp/all.txt > sample_counts.txt
 
 
