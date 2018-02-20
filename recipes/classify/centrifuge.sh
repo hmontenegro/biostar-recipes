079a8868703c9982a50e23c58cb2e98f9d73dac6
Istvan Albert <istvan.albert@gmail.com>
2018-02-20 15:37:25 -0500

    Update by ialbert on Tue Feb 20 15:37:25 EST 2018 from yolo.local

--- recipes/classify/centrifuge.sh
+++ recipes/classify/centrifuge.sh
@@ -2,14 +2,30 @@
 # The table of contents for the data.
 INPUT={{reads.toc}}
 
+# The library type.
+LIBRARY={{library.value}}
+
+# The sorted file list.
+FILES=files.txt
+
+# Create the sorted filelist.
+cat ${INPUT}| egrep "fastq|fq" | sort > $FILES
+
 # The Centrifuge index.
 INDEX=/export/refs/centrifuge/p_compressed+h+v
 
 # Create the reports file.
 mkdir -p results
 
-# Perform a classification on each sequencing file.
-cat ${INPUT} | egrep "fastq|fq" | parallel -j 1 "centrifuge -x  $INDEX {} > results/{1/.}.rep"
+# Choose the right classifier depending on the library setting.
+if [ ${LIBRARY} == "PE" ]; then
+    # Paired end classification.
+    cat ${FILES} | parallel -N 2  -j 4 "centrifuge -x  $INDEX -1 {1} -2 {2} > results/{1/.}.rep"
+else
+    # Single end classification.
+    cat ${FILES} | parallel -j 4 "centrifuge -x  $INDEX -U {} > results/{1/.}.rep"
+fi
+
 
 # Reformat the results as a report.
 for FNAME in results/*.rep; do
