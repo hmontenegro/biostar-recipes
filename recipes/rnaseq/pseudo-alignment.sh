5a2067fe2206ae81ad22f83fe95cddd514fffeb6
aswathy <aswathyseb@gmail.com>
2018-02-21 11:03:26 -0500

    Update by asebastian on Wed Feb 21 11:03:26 EST 2018 from Aswathys-iMac.local

--- recipes/rnaseq/pseudo-alignment.sh
+++ recipes/rnaseq/pseudo-alignment.sh
@@ -6,159 +6,81 @@ INPUT={{reads.toc}}
 # Reference Transcriptome.
 TRANSCRIPTS={{transcripts.value}}
 
-# Library protocol.
+# Library type.
 LIBRARY={{library.value}}
-
-# Library type
-LIBRARY_TYPE={{library_tpe.value}}
-
-# PE read orientation
-ORIENTATION={{orientation.value}}
-
-# Tool to be used.
-TOOL={{tool.value}}
 
 # Fraction to sub-sample the data.
 FRACTION={{fraction.value}}
 
-# Estimated average fragment length.
-FRAG_LEN={{fragment_length.value}}
-
-# Estimated standard deviation of fragment length.
-FRAG_SD={{fragment_sd.value}}
-
 # Run log to redirect unwanted output.
-RUNLOG=runlog/runlog.
+RUNLOG=runlog/runlog.txt
 
 # This directory should already exist.
 mkdir -p runlog
 
-# Directory with results.
+# Directory with Kallisto results.
 mkdir -p results
 
 # Directory with intermediate files.
 mkdir -p tmp
 
-# Directory with sub-sampled data.
-READS=reads
-mkdir -p $READS
-
-# The name of the input files.
-FILES=${READS}/files.txt
-
-#Create file list
-cat ${INPUT} | egrep 'fastq|fq'|sort >${FILES}
-
 # Sub-sample data.
 if [ ${FRACTION} != 1 ]; then
+    READS=reads
+    mkdir -p $READS
 
-    # The random seed for sampling.
-    SEED=$((1 + RANDOM % 1000))
+    # Subsample data.
+    echo "Randomly sampling $FRACTION fraction of data"
+    cat ${INPUT} | egrep "fastq|fq" | sort |  parallel "seqtk sample -s 11 {} $FRACTION >$READS/{/.}.fq"
 
-    # Generate a random sample of each input file.
-    echo "Sampling fraction=$FRACTION of data with random seed=$SEED"
-    cat ${FILES} |  parallel "seqtk sample -s $SEED {} $FRACTION >$READS/{/.}.fq"
+    # The name of the new table of contents.
+    INPUT=$READS/toc.txt
 
     # Create table of contents with sub-sampled data.
-    ls -1 $READS/*.fq > ${FILES}
+    ls -1 $READS/*.fq > $INPUT
 fi
 
-# Index directory.
+# Kallisto index directory.
 INDEX_DIR={{runtime.local_root}}/indices
 mkdir -p ${INDEX_DIR}
 
 # Kallisto index.
-KINDEX=${INDEX_DIR}/{{transcripts.uid}}_kallisto.idx
+INDEX=${INDEX_DIR}/{{transcripts.uid}}.idx
 
-# Salmon index.
-SINDEX=${INDEX_DIR}/{{transcripts.uid}}_salmon.idx
-
-# Build the Kallisto index if needed.
-if [ "$TOOL" == "kallisto" ] && [ ! -f "$KINDEX" ]; then
-
+# Build the Kallisto index if it does not already exist.
+if [ ! -f ${INDEX} ]; then
     echo "Building the kallisto index."
-    kallisto index -i ${SINDEX} ${TRANSCRIPTS} >> $RUNLOG 2>&1
+    kallisto index -i ${INDEX} ${TRANSCRIPTS} >> $RUNLOG 2>&1
+else
+    echo "Found an existing kallisto index."
 fi
 
-# Build the Salmon index if needed.
-if [ "$TOOL" == "salmon" ] && [ ! -f "$SINDEX" ]; then
+# Calculate abundances using kallisto.
+if [ ${LIBRARY} == "PE" ]; then
+    echo "Running kallisto quant algorithm in paired end mode."
+    cat ${INPUT}| egrep "fastq|fq" | sort | parallel -N 2  -j 4 kallisto quant -o results/{1/.}.out  -i ${INDEX} {1} {2} >> $RUNLOG 2>&1
+else
+    # Obtain the additional parameters for single end mode.
 
-    echo "Building the salmon index."
-    salmon index -i ${SINDEX} -t ${TRANSCRIPTS} -p 2 >> $RUNLOG 2>&1
+    # Estimated average fragment length.
+    FRAG_LEN={{fragment_length.value}}
+
+    # Estimated standard deviation of fragment length.
+    FRAG_SD={{fragment_sd.value}}
+
+    # Run kallisto in single end more.
+    echo "Running kallisto quant algorithm in single end mode."
+    cat ${INPUT}| egrep "fastq|fq" | sort | parallel -j 4 kallisto quant -o results/{1/.}.out  -i ${INDEX} --single -l ${FRAG_LEN} -s ${FRAG_SD} {} >> $RUNLOG 2>&1
 fi
-
-# Kallisto in single end mode.
-if [ "$TOOL" == "kallisto" ] && [ "$LIBRARY" == "SE" ]; then
-
-    echo  "Estimating transcript abundances using $TOOL in single end mode."
-    cat ${INPUT}| egrep "fastq|fq" | sort | parallel -j 4 kallisto quant -o results/{1/.}.out  -i ${KINDEX} --single
-    -l ${FRAG_LEN} -s ${FRAG_SD} {} >> $RUNLOG 2>&1
-fi
-
-# Kallisto in paired end mode.
-if [ "$TOOL" == "kallisto" ] && [ "$LIBRARY" == "PE" ]; then
-
-    echo "Estimating transcript abundances usinf $TOOL in paired end mode."
-
-    if [ "$LIBRARY_TYPE" == "unstranded" ]; then
-        cat ${INPUT}| egrep "fastq|fq" | sort | parallel -N 2  -j 4 kallisto quant -o results/{1/.}.out  -i ${KINDEX} {1}
-     {2} >> $RUNLOG 2>&1
-
-    else
-        # run kallisto in stranded mode.
-        cat ${INPUT}| egrep "fastq|fq" | sort | parallel -N 2  -j 4 kallisto quant -o results/{1/.}.out --${LIBRARY_TYPE} -i ${KINDEX} {1}
-     {2} >> $RUNLOG 2>&1
-     fi
-
-fi
-
-# Salmon in single end mode.
-if [ "$TOOL" == "salmon" ] && [ "$LIBRARY" == "SE" ]; then
-
-    echo  "Estimating transcript abundances using $TOOL in single end mode."
-    cat ${INPUT}| egrep "fastq|fq" | sort | parallel -j 4 salmon quant -o results/{1/.}.out  -i ${SINDEX} --libType ${LIBRARY_TYPE}
-     -r {} >> $RUNLOG 2>&1
-fi
-
-
-# Salmon in paired end mode.
-if [ "$TOOL" == "salmon" ] && [ "$LIBRARY" == "PE" ]; then
-
-    echo  "Estimating transcript abundances using $TOOL in single end mode."
-
-    if ["${LIB_TYPE}" == "fr-stranded" ]; then
-        LIB_TYPE_STR=${ORIENTATION}"SF"
-    fi
-
-    if ["${LIB_TYPE}" == "rf-stranded" ]; then
-        LIB_TYPE_STR=${ORIENTATION}"SR"
-    fi
-
-    cat ${INPUT}| egrep "fastq|fq" | sort | parallel -N 2 -j 4 salmon quant -o results/{1/.}.out  -i ${SINDEX}
-    --libType ${LIB_TYPE_STR} -1 {1} -2 {2} >> $RUNLOG 2>&1
-
-fi
-
 
 # Create a combined count table for all samples.
 
-if [ "$TOOL" == "kallisto" ]; then
-    # Directory name is the sample name.
-    # Rename est_count column to sample name and extract it into a new file.
-    ls -d results/* | cut -f 2 -d "/" | parallel "sed -e 1s/est_counts/{.}/ results/{}/abundance.tsv | cut -f 4 >tmp/{}_counts.txt"
-fi
-
-if [ "$TOOL" == "salmon" ]; then
-    # Directory name is the sample name.
-    # Rename NumReads column to sample name and extract it into a new file.
-    ls -d results/* | cut -f 2 -d "/" | parallel "sed -e 1s/NumReads/{.}/ results/{}/quant.sf | cut -f 5 >tmp/{}_counts.txt"
-fi
+# Directory name is the sample name.
+# Rename est_count column to sample name and extract it into a new file.
+ls -d results/* | cut -f 2 -d "/" | parallel "sed -e 1s/est_counts/{.}/ results/{}/abundance.tsv | cut -f 4 >tmp/{}_counts.txt"
 
 # Combine all counts into a single file.
 paste tmp/*counts.txt > tmp/all.txt
 
 # Add  transcript ids to the counts.
 ls -d results/* | head -1 | parallel cat {}/abundance.tsv | cut -f 1 | paste - tmp/all.txt > combined_abundance.txt
-
-
-
