26268915a1d9534d54b4625b9577686deae88c56
aswathy <aswathyseb@gmail.com>
2018-02-21 23:18:13 -0500

    Update by aswathyseb on Wed Feb 21 23:18:13 EST 2018 from MacBook-Air.local

--- recipes/usfish/trim.sh
+++ recipes/usfish/trim.sh
@@ -1,8 +1,8 @@
 # Input parameters.
 
-INPUT={{reads.value}}
+INPUT={{reads.toc}}
 SAMPLESHEET={{samplesheet.value}}
-QUALTIY={{quality.value}}
+QUALITY={{quality.value}}
 MIN_LEN={{min_length.value}}
 KMER_LEN={{kmer_length.value}}
 
@@ -16,14 +16,18 @@ mkdir -p trimmed
 # Directory that holds merged reads.
 mkdir -p merged
 
-# Directory with reads that are filtered for Ns
+# Directory with reads that are filtered for Ns.
 mkdir -p filtered
 
+# Get data path.
+DATA_DIR=$(dirname $(cat $INPUT| head -1 ))
+
 # trim forward and reverse primer and then trim by quality.
+echo " Removing primers and trimming by quality."
 
-cat $SAMPLESHEET | parallel -j 4  --header : --colsep '\t' bbduk.sh in1={read1} in2={read2}  out1=trimmed/{sample}_R1.fq.gz out2=trimmed/{sample}_R2.fq.gz \
-literal={fwd_primer},{rev_primer} ktrim=l k=$KMER_LEN hdist=1 qtrim=r trimq=$QUALITY minlength=$MIN_LEN overwrite=true 2>>$RUNLOG
-
+cat $SAMPLESHEET | parallel -j 4  --header : --colsep '\t' bbduk.sh in1=${DATA_DIR}/{read1} in2=${DATA_DIR}/{read2} \
+out1=trimmed/{sample}_R1.fq.gz out2=trimmed/{sample}_R2.fq.gz literal={fwd_primer},{rev_primer} ktrim=l k=$KMER_LEN \
+hdist=1 qtrim=r trimq=$QUALITY minlength=$MIN_LEN overwrite=true 2>>$RUNLOG
 
 # Merge trimmed reads.
 echo "Merging  paired end reads."
@@ -31,6 +35,6 @@ ls trimmed/*.fq.gz | sed 's/_R1.fq.gz//g' | parallel -N 2 -j 4 bbmerge.sh in1={1
 
 # Remove reads with Ns.
 echo "Filtering reads with Ns."
-ls merged/*.fq.gz | sed 's/_merged.fq.gz//g' | parallel -j 4 bbduk.sh in={}_merged.fq.gz out=filtered/{}_filtered.fq.gz maxns=0 2>>$RUNLOG
+ls merged/*.fq.gz | sed 's/_merged.fq.gz//g' | parallel -j 4 bbduk.sh in={}_merged.fq.gz out=filtered/{/}_filtered.fq.gz maxns=0 2>>$RUNLOG
 
 
