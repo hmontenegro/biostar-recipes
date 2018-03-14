04a3cbde49e9febc5782175dd05df448949eaba8
Istvan Albert <istvan.albert@gmail.com>
2018-03-14 12:06:40 -0400

    Update by ialbert on Wed Mar 14 12:06:40 EDT 2018 from yolo.local

--- recipes/code/centrifuge.py
+++ recipes/code/centrifuge.py
@@ -19,10 +19,12 @@ Usage:
                    
 """
 
-import sys, os, csv
+import csv
+import os
+import sys
+
 
 def plot(data, outfile, header, title='Title', width=0.35, opacity=0.8):
-
     import matplotlib.pyplot as plt
 
     objects = {}
@@ -35,59 +37,76 @@ def plot(data, outfile, header, title='Title', width=0.35, opacity=0.8):
     ax, fig = plt.subplot(), plt.gcf()
 
     for idx, x_val in enumerate(objects.items()):
-        #TODO: needs to match y_pos shape
+        # TODO: needs to match y_pos shape
         d = [float(x) for y in x_val[1] for x in y]
         print(d, y_pos, len(d), len(y_pos), x_val)
         # Shift to accommodate more than one bar
         # max number of bars = len (colors) = 7
-        #if color > 0:
+        # if color > 0:
         y_pos = y_pos + width
         plt.barh(y_pos + width, d, width,
-                alpha=opacity,
+                 alpha=opacity,
                  # Label and color need to come from filename.
-                #color=colors[color],
-                #label=n if not EASYMAP.get(n) else EASYMAP[n],
-                align = 'center')
+                 # color=colors[color],
+                 # label=n if not EASYMAP.get(n) else EASYMAP[n],
+                 align='center')
 
     plt.yticks(y_pos + width, objects)
-    #plt.xlabel(f'{x_label}', weight='bold')
+    # plt.xlabel(f'{x_label}', weight='bold')
     plt.title(f'{title}', fontsize=10, weight='bold')
     handles, labels = ax.get_legend_handles_labels()
     plt.legend(handles, labels, loc='right', bbox_to_anchor=(0.15, -0.1),
-                  prop={'size': 8})
+               prop={'size': 8})
     plt.tight_layout()
-    #fig.set_size_inches(9, 5.5)
+    # fig.set_size_inches(9, 5.5)
     plt.show()
-    #plt.savefig(outfile)
+    # plt.savefig(outfile)
     print(f"Plot saved to :{os.path.abspath(outfile)}")
     print(objects)
     print(objects)
-    1/0
+    1 / 0
 
     return
 
 
-def tabulate(files, rank, column, keyid='taxID', cutoff=1):
+# We have to hardcode since the files does not have headers
+# percent clade_count taxon_count rank_code taxid nameTAXIDX = INPUT_HDRS['taxid']
+
+def print_table(table, files, rank='', header=False):
+    '''
+    Prints table at a certain rank
+    '''
+    basenames = [os.path.basename(fname) for fname in files]
+    basenames = [os.path.splitext(fname)[0] for fname in basenames]
+    outheader = [ "Name", "taxid", "rank" ]  + basenames
+
+    if header:
+        print("\t".join(outheader))
+
+    # Filter by ranks if required
+    cond2 = lambda row: (row[2]) == rank
+    table = list(filter(cond2, table)) if rank else table
+
+    for row in table:
+        row = map(str, row)
+        print("\t".join(row))
+
+def tabulate(files, rank='', rankidx=3, keyidx=4, cutoff=1):
     "Summarize result found in data_dir by grouping them."
-
-    # Find the headers
-    fname = files[0]
-    stream = csv.reader(open(fname, 'rt'), delimiter="\t")
-    header = next(stream)
-
-    # Find the rank index
-    rankidx = header.index(rank) if rank in header else -1
 
     # Collect all data into a dictionary keyed by keyID
     storage = []
     for fname in files:
-        stream = csv.DictReader(open(fname, 'rt'), delimiter="\t")
+        stream = csv.reader(open(fname, 'rt'), delimiter="\t")
+
+        # Keep only known rank codes
+        stream = filter(lambda x: x[rankidx] != '-', stream)
         data = dict()
         for row in stream:
-            data[row[keyid]] = row
+            data[row[keyidx]] = [elem.strip() for elem in row]
         storage.append(data)
 
-    # Collect all keys across all data.
+    # Collect all taxid keys across all data.
     allkeys = {}
     for data in storage:
         allkeys.update(data)
@@ -95,29 +114,33 @@ def tabulate(files, rank, column, keyid='taxID', cutoff=1):
     # The final table that can be printed and further analyzed
     table = []
     for key, fields in allkeys.items():
-        collect = [ fields[name] for name in header[:4]]
+        collect = list(reversed(fields[3:]))
         for data in storage:
-            fields = data.get(key, {})
-            value = int(fields.get(column, 0))
+            # The first three elements are needed
+            # add the first fields if needed
+            value = data.get(key, [0])[0]
+            value = float(value)
             collect.append(value)
         table.append(collect)
 
     # Filter table by cutoffs
-    cond1 = lambda row: sum(row[4:]) > cutoff
+    cond1 = lambda row: sum(row[3:]) > cutoff
     table = list(filter(cond1, table))
 
-    # Filter by ranks if required
-    cond2 = lambda row: (row[rankidx]) == rank
-    table = list(filter(cond2, table)) if rankidx > -1 else table
+    # Sort by reverse of the abundance.
+    compare = lambda row: (row[2], sum(row[3:]))
+    table = sorted(table, key=compare, reverse=True)
 
-    # Print the table
-    basenames = [os.path.basename(fname) for fname in files]
-    basenames = [os.path.splitext(fname)[0] for fname in basenames]
-    outheader = header[:4] + basenames
-    print("\t".join(outheader))
-    for row in table:
-        row = map(str, row)
-        print ("\t".join(row))
+    if rank:
+        # Print selected rank
+        print_table(table, files=files, rank=rank, header=True)
+    else:
+        # Print all ranks
+        print_table(table, files=files, rank='S', header=True)
+        print_table(table, files=files, rank='G')
+        print_table(table, files=files, rank='F')
+        print_table(table, files=files, rank='C')
+        print_table(table, files=files, rank='D')
 
     return table
 
@@ -131,14 +154,10 @@ def main():
                         help='a file list')
 
     parser.add_argument('--rank', dest='rank',
-                       help="Filter summary report to a taxonomic rank default.",
-                       type=str, default='')
+                        help="Filter summary report to a taxonomic rank default.",
+                        type=str, default='')
 
-    parser.add_argument('--column', dest='column', default='numReads',
-                       help="Column to combine across all samples and put in summary report default=%(default)s.",
-                       type=str)
-
-    parser.add_argument('--cutoff', dest='cutoff', default=10,
+    parser.add_argument('--cutoff', dest='cutoff', default=1,
                         help="The sum of rows have to be larger than the cutoff to be registered default=%(default)s.",
                         type=int)
 
@@ -151,13 +170,12 @@ def main():
                         type=str)
 
     if len(sys.argv) == 1:
-        sys.argv.extend( [ 'data/SRR1972972_1.tsv', 'data/SRR1972972_2.tsv' ])
+        sys.argv.extend(['data/1000-MiFish_R1.fq.txt', 'data/1000-MiFish_R2.fq.txt'])
 
     args = parser.parse_args()
 
-
     results = tabulate(files=args.files, rank=args.rank,
-                       column=args.column, cutoff=args.cutoff)
+                       cutoff=args.cutoff)
 
     '''
     if args.plot:
@@ -166,6 +184,7 @@ def main():
             plot(data=values, title=f'{rank}',outfile=args.prefix + '.png',
                  header=generate_header(files))
     '''
+
 
 if __name__ == '__main__':
     main()
