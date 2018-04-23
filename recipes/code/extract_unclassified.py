baebdc693dfc1c3993ca2c25ce2a327a1c9f2a9a
Natay <b.natay@gmail.com>
2018-04-23 13:38:49 -0400

    Update by natay on Mon Apr 23 13:38:49 EDT 2018 from natay

--- recipes/code/extract_unclassified.py
+++ recipes/code/extract_unclassified.py
@@ -7,68 +7,48 @@ import os
 DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
 
 
+def single_end(stream, seen=(), output=None):
+    """
+    Takes a fastq stream and a set of already seen/classified reads
+    and puts what is not seen/unclassified into a fastq file inside of outdir.
 
+    """
 
+    dirname = output or os.path.dirname(stream.path)
 
-def paired_end(stream1, stream2, seen=(), output_dir=None):
-
-    input_stream = zip(stream1, stream2)
-    dirname = output_dir or os.path.dirname(stream1.path)
-
-    path1 = os.path.join(dirname,f'Unclassified_{stream1.name}')
-    path2 = os.path.join(dirname,f'Unclassified_{stream2.name}')
-    output1 = open(path1, 'w')
-    output2 = open(path2, 'w')
-
-    for id1, id2, in input_stream:
-
-        seq1, seq2 = next(input_stream)
-        tmp1, tmp2 = next(input_stream)
-        qual1, qual2 = next(input_stream)
-
-        if id1.strip() not in seen:
-            output1.write(f"{id1}{seq1}{tmp1}{qual1}")
-        if id2.strip() not in seen:
-            output2.write(f"{id2}{seq2}{tmp2}{qual2}")
-
-    return path1, path2
-
-
-def single_end(stream, seen=(), output_dir=None):
-
-    dirname = output_dir or os.path.dirname(stream.path)
-    path = os.path.join(dirname, f'Unclassified_{stream.name}')
+    path = os.path.join(dirname, f'Unclassified_{os.path.basename(stream.name)}')
 
     output = open(path, 'w')
 
     for rid in stream:
+        # Need to further clean line to extract read id.
+        check = rid.split()[0].replace('@','')
+
         seq = next(stream)
         tmp = next(stream)
         qual = next(stream)
 
-        if id not in seen:
+        if check not in seen:
             output.write(f"{rid}{seq}{tmp}{qual}")
 
     return path
 
 
-def extract(stream1, report_file, stream2=None):
+def extract(stream1, report_file, stream2=None, output=None):
 
     seen = set()
     source = open(report_file, 'r')
     for line in source:
-        seen.add(line.split()[0])
+        seen.add(line.split()[0].strip())
 
     if stream2:
-
-        unclassified = [single_end(stream=s, seen=seen) for s in (stream1, stream2)]
+        # Handle pair-end reads with list-comp
+        unclassified = [single_end(stream=s, seen=seen, output=output) for s in (stream1, stream2)]
     else:
-        unclassified = single_end(stream=stream1, seen=seen)
+        unclassified = [single_end(stream=stream1, seen=seen, output=output)]
 
-
+    # Returns list of paths to new fastq files with unclassified reads.
     return unclassified
-
-
 
 
 def main():
@@ -77,30 +57,33 @@ def main():
 
     parser = ArgumentParser()
 
-    parser.add_argument('files', metavar='FILES', type=str, nargs='+',
-                        help='Files to be plotted')
+    parser.add_argument('files', metavar='FILES', type=str, nargs=2,
+                        help='Fastq file(s) used to generate the report file')
 
-    parser.add_argument('--sample_sheet', dest='sample_sheet', type=str, required=True,
-                        help='Sample sheet with metabarcoding data.')
+    parser.add_argument('--report_file', dest='report', type=str, required=True,
+                        help='Generated report file.')
 
-    parser.add_argument('--input', dest='input', type=str, required=True,
-                        help='Directory with the input reads in Fastq format')
-
-    parser.add_argument('--results', dest='results', type=str, required=True,
-                        help='Directory with the classification results')
+    parser.add_argument('--output', dest='output', type=str,
+                        help='Directory to store the unclassified fastq file.')
 
     if len(sys.argv) == 1:
-        sys.argv.extend([f'--sample_sheet={os.path.join(DATA_DIR, "sample-sheet.csv")}',
-                         f'--input={DATA_DIR}',
-                         f'--results={DATA_DIR}'])
+        report_file = os.path.join(DATA_DIR, '1000-MiniXFish.rep')
+        files = ['1000-MiniXFish_S2_L001_R1_001.fastq.gz', '1000-MiniXFish_S2_L001_R2_001.fastq.gz']
+        files = [os.path.join(DATA_DIR, f) for f in files]
+
+        sys.argv.extend(files + [f'--report_file={report_file}', f'--output={DATA_DIR}'])
 
     args = parser.parse_args()
 
+    stream1 = open(args.files[0], 'r')
+    stream2 = None if len(args.files) <= 1 else open(args.files[1], 'r')
 
-    print(extract())
-    # Check if readID in the sample.rep file is in read1 file or read2 file , make new fastq file if not.
-
-
+    outputs = extract(stream1=stream1,
+                        stream2=stream2,
+                        report_file=args.report,
+                        output=args.output)
+    for o in outputs:
+        print(o)
 
 
 if __name__ == '__main__':
